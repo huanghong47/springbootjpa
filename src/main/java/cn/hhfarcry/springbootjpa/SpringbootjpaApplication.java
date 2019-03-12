58a6bd6c8ec2bfde8c9bbf282e5b9dd00bd21e7a
huanghong <2393187992@qq.com>
2019-03-12 11:25:01 +0800

    无

--- src/main/java/cn/hhfarcry/springbootjpa/SpringbootjpaApplication.java
+++ src/main/java/cn/hhfarcry/springbootjpa/SpringbootjpaApplication.java
@@ -2,9 +2,18 @@ package cn.hhfarcry.springbootjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-public class SpringbootjpaApplication {
+public class SpringbootjpaApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootjpaApplication.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootjpaApplication.class, args);
